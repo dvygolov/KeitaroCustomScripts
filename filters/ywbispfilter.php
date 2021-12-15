ba5c216434e38314bba915d718f8ca223db15c09
Daniel Vygolov <rattack@gmail.com>
2021-12-16 00:03:51 +0400

    Now ISP Filter can store ISP value and can load isp-base from the same directory

--- filters/ywbispfilter.php
+++ filters/ywbispfilter.php
@@ -5,13 +5,14 @@ use Core\Filter\AbstractFilter;
 use Core\Locale\LocaleService;
 use Traffic\Model\StreamFilter;
 use Traffic\RawClick;
-require_once 'geoip2.phar';
+require_once '/var/www/keitaro/application/filters/geoip2.phar';
 use GeoIp2\Database\Reader;
 
 /*
 Кастомный фильтр для Кейтаро для фильтрации по ISP, используя бесплатную базу MaxMind ASN
 Скопировать файл фильтра в папку application\filters затем перелогиниться в трекер
 Устанавливаете фильтр в потоке, в поле пишите (не)нужные вам ISP через запятую БЕЗ пробела
+Статья по работе с фильтром: https://vk.com/@yellowweb-dobavlyaem-v-keitaro-kastomnyi-filtr-dlya-filtracii-po-isp-p
 ©2021 by Yellow Web
  */
 class ywbispfilter extends AbstractFilter
@@ -31,14 +32,16 @@ class ywbispfilter extends AbstractFilter
 
     public function isPass(StreamFilter $filter, RawClick $rawClick)
     {
+    	$sub_id_num=7; //Поменяйте здесь номер суб-метки для записи в неё ISP
 		$isp_str = $filter->getPayload(); //получаем все разрешённые/запрещённые ISP
 		$isps = explode(',',$isp_str); //
-		$reader = new Reader(__DIR__.'/GeoLite2-ASN.mmdb');
+		$reader = new Reader('/var/www/keitaro/application/filters/GeoLite2-ASN.mmdb');
 		$ip = $rawClick->getIpString();
 		$record = $reader->asn($ip);
 		$cur_isp=$record->autonomousSystemOrganization;
-		//file_put_contents(__DIR__."/isplog.txt",$ip." ".$cur_isp."\n",FILE_APPEND);
+		//file_put_contents("/var/www/keitaro/application/filters/isplog.txt",$ip." ".$cur_isp."\n",FILE_APPEND); //для отладки
 		
+
 		$hasMatches = false;
 		foreach($isps as $isp){
 			if(!empty(stristr($cur_isp,$isp))){
@@ -46,6 +49,25 @@ class ywbispfilter extends AbstractFilter
 				break;
 			}
 		}
+
+		//записываем у клика в выбранную суб-метку данные об ISP
+		$subid=$rawClick->getSubId();
+		//получаем текущий адрес
+		$sreq=$this->getServerRequest();
+		$uri=explode('?',urldecode($sreq->getUri()))[0];
+		//file_put_contents("/var/www/keitaro/application/filters/isplog.txt",$uri."\n",FILE_APPEND); //для отладки
+		$params=array();
+		$params['_update_tokens']=1;
+		$params['sub_id']=$subid;
+		$params['sub_id_'.$sub_id_num]=$cur_isp;
+        $qs=http_build_query($params);
+        $url=$uri."?{$qs}";
+		$ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $res=curl_exec($ch);
+        $result=json_decode($res,true);
+        curl_close($ch);
 		
 		return ($filter->getMode() == StreamFilter::ACCEPT && $hasMatches)
             || ($filter->getMode() == StreamFilter::REJECT && !$hasMatches);
