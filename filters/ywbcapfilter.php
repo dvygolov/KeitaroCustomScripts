8552fe1d8f6f8489c558e83df9cbc39c331d6687
Yellow Web <rattack@gmail.com>
2022-05-28 15:22:03 +0400

    Redis caching for cap filter

--- filters/ywbcapfilter.php
+++ filters/ywbcapfilter.php
@@ -12,9 +12,8 @@ use Traffic\RawClick;
 Устанавливаете фильтр в потоке, в поле пишите кап.
 Фильтр проверяет общее кол-во лидов по ВСЕМ проклам в потоке
 Если общее кол-во лидов за сегодня по этим проклам меньше заданного капа - пропускаем траф.
-При больших объёмах трафика крайне рекомендуется создать в папке application\filters папку capFilterCache 
-и дать туда права на запись для Кейтаро, чтобы фильтр каждый раз не лез в БД
-©2020 by Yellow Web
+В качестве механизма для кеширования используется Redis
+©2022 by Yellow Web
  */
 class ywbcapfilter extends AbstractFilter
 {
@@ -40,27 +39,29 @@ class ywbcapfilter extends AbstractFilter
         //Дальше ничего не трогаем, если не умеем!!!
 		date_default_timezone_set($tz);
 
-        $cacheDir="/var/www/keitaro/application/filters/capFilterCache";
         $cachetime = 300; // 5 минут
         
+        $ktRedis = \Traffic\Redis\Service\RedisStorageService::instance();
+        $redis = $ktRedis->getOriginalClient();
+
         $ch = curl_init();
         $streamId=$filter->getStreamId();
-        $cachefile = $cacheDir.'/stream-'.$streamId.'.json';
-		if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
-            $json = file_get_contents($cachefile);
-            $streamParams = json_decode($json, true);
-        } else {
+        $cachekey = 'ywbCapFilter-stream-'.$streamId;
+        $res = $redis->get($cachekey);
+        if ($res===false){
             //запрашиваем все данные по потоку, чтобы вынуть из него идентификаторы офферов
             $fullAddress=$apiAddress.'streams/'.$streamId;
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
             curl_setopt($ch, CURLOPT_URL, $fullAddress);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Api-Key: '.$apiKey));
             $res = curl_exec($ch);
-            $streamParams = json_decode($res,true);
-            //кешируем на диск
-            $json = json_encode($streamParams,JSON_PRETTY_PRINT);
-            file_put_contents($cachefile, $json);
+            $redis->set($cachekey, $res, ['nx', 'ex'=>$cachetime]);
+		    /* file_put_contents("/var/www/keitaro/application/filters/capFilterCache/stream_from_db.txt", $res); //отладка */
         }
+        /* else{ */
+		    /* file_put_contents("/var/www/keitaro/application/filters/capFilterCache/stream_from_redis.txt", $res); //отладка */
+        /* } */
+        $streamParams = json_decode($res,true);
 
 		//вынимаем идентификаторы лендов
 		$landingIds=[];
@@ -70,12 +71,10 @@ class ywbcapfilter extends AbstractFilter
 		}
 
         sort($landingIds);
-        $cachefile = $cacheDir.'/landings-'.implode(",",$landingIds).'.json';
-	
-		if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
-            $json = file_get_contents($cachefile);
-            $report = json_decode($json, true);
-        } else {
+
+        $cachekey = 'ywbCapFilter-landings-'.implode(",",$landingIds);
+        $res = $redis->get($cachekey);
+		if ($res===false) {
             //запрашиваем отчёт по кол-ву лидов у наших лендингов за сегодня
             $params = [
                 'columns' => [],
@@ -96,12 +95,14 @@ class ywbcapfilter extends AbstractFilter
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));		
             $res=curl_exec($ch);
-            $report=json_decode($res,true);
-            //кешируем на диск
-            $json = json_encode($report,JSON_PRETTY_PRINT);
-            file_put_contents($cachefile, $json);
+            $redis->set($cachekey, $res, ['nx', 'ex'=>$cachetime]);
+		    /* file_put_contents("/var/www/keitaro/application/filters/capFilterCache/rep_from_db.txt", $res); //отладка */
         }
-		
+        /* else{ */
+		    /* file_put_contents("/var/www/keitaro/application/filters/capFilterCache/rep_from_redis.txt", $res); //отладка */
+        /* } */
+        $report = json_decode($res, true);
+
 		$totalLeads=0;
 		foreach($report['rows'] as $row)
 		{
