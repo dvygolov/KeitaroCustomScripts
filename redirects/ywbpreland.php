cc70e8db8ecdcd46b2e77493163fef4df509d4ed
Daniel Vygolov <rattack@gmail.com>
2023-06-16 15:58:20 +0400

    Added base tag

--- redirects/ywbpreland.php
+++ redirects/ywbpreland.php
@@ -1,5 +1,5 @@
 <?php
-//Скрипт кастомного Action для трекера Кейтаро by Yellow Web ©2020
+//Скрипт кастомного Action для трекера Кейтаро by Yellow Web ©2023
 //Экшн позволяет использовать любую проклу из интернета, как если бы она была у вас локально загружена в трекер
 //Использование:
 //Создать Лендинг, указать в Схеме Действие - Украсть проклу, указать адрес по которому висит прокла
@@ -13,27 +13,30 @@ class ywbPreland extends AbstractAction
     protected $_name = 'Украсть проклу';
     protected $_weight = 100;
 
-	public function getType()
+    public function getType()
     {
-        return self::TYPE_OTHER; 
-    }    
-	
-	protected function _execute()
+        return self::TYPE_OTHER;
+    }
+
+    protected function _execute()
     {
 
-		$url = $this->getActionPayload();
-		$curl = curl_init();
-		$optArray = array(
-				CURLOPT_URL => $url,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_SSL_VERIFYPEER => false,
-				CURLOPT_FOLLOWLOCATION => true);
-		curl_setopt_array($curl, $optArray);
-		$html = curl_exec($curl);
-		$info = curl_getinfo($curl);
-		curl_close($curl);
-		$html=preg_replace('/(<a[^>]+href=\")([^"]+)/i','$1{offer}',$html);
-		$html=$this->processMacros($html);
-		$this->setContent($html);
+        $url = $this->getActionPayload();
+        $curl = curl_init();
+        $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_FOLLOWLOCATION => true
+        );
+        curl_setopt_array($curl, $optArray);
+        $html = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+        $html = preg_replace('/(<a[^>]+href=\")([^"]+)/i', '$1{offer}', $html);
+        $html = preg_replace("/<head>/", '<head><base href="' . $url . '/">', $html); //need to add base tag
+        $html = $this->processMacros($html);
+        $this->setContent($html);
     }
 }
+
