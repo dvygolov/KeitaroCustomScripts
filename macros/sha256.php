a38c2bcbdb00ae495a120acf7c30baaa8c2b8a80
Daniel Vygolov <rattack@gmail.com>
2023-06-23 12:04:40 +0400

    Macros for calculating sha256 hash for any sub_id_N. Useful in S2S postbacks, for example, FB Conversion API

---
+++ macros/sha256.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Macros;
+
+use Traffic\Model\BaseStream;
+use Traffic\RawClick;
+use Traffic\Macros\AbstractClickMacro;
+
+/*
+  Macro for Keitaro Tracker(https://yellowweb.top/keitaro) 
+  This macro is useful for S2S-postbacks when you do not need to send raw values but their hashes.
+  Macro has 1 parameter - sub id number.
+  Usage: {sha256:1} will return sha256 hash for sub_id_1
+  Author: Yellow Web (http://yellowweb.top)
+ */
+
+class sha256 extends AbstractClickMacro
+{
+  public function process(BaseStream $stream, RawClick $click, $subNumber)
+  {
+    return hash("sha256", $click->getSubIdN($subNumber));
+  }
+}
