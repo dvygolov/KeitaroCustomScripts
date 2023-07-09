b116f9a5ae6bf38b48b2fd41651dee4f89eeb979
Yellow Web <rattack@gmail.com>
2023-07-09 18:50:51 +0400

    Deepl translate v3.0, new lib and composer install

--- redirects/Deepl/ywbdeeplbg.php
+++ redirects/Deepl/ywbdeeplbg.php
@@ -1,30 +1,22 @@
 <?php
 
-require_once('DeepLException.php');
-require_once('ConnectionException.php');
-require_once('DocumentTranslationException.php');
-require_once('DocumentHandle.php');
-require_once('DocumentStatus.php');
-require_once('BackoffTimer.php');
-require_once('HttpClient.php');
-require_once('LanguageCode.php');
-require_once('Translator.php');
-require_once('TranslateDocumentOptions.php');
-require_once('TranslatorOptions.php');
-require_once('TranslateTextOptions.php');
-require_once('TextResult.php');
-
-use DeepL\Translator;
+require_once(__DIR__.'/vendor/autoload.php');
 
 class DeeplBackgroundTranslator
 {
   protected $deeplApiKey  = "";  //ENTER YOUR DEEPL API KEY HERE
+  protected $translator;
 
-  function __construct()
-  {
-    echo "Deepl Background Translator v2.0 by Yellow Web\n";
+  function __construct(){
+    echo "Deepl Background Translator v3.0 by Yellow Web\n";
     echo "Running...\n";
     ini_set('default_socket_Timeout', -1); // PHP configuration does not time out
+
+    $logfile = '/home/keitaro/deepl/deepllib.log';
+    $channel = 'events';
+    $logger  = new SimpleLog\Logger($logfile, $channel);
+    $logger->setOutput(true);
+    $this->translator = new \DeepL\Translator($this->deeplApiKey, ["logger"=>$logger]);
     $redis = new Redis();
     $redis->connect("127.0.0.1", 6379);
     $redis->setOption(Redis::OPT_READ_TIMEOUT, -1); // redis mode does not time out. Recommended
@@ -35,8 +27,8 @@ class DeeplBackgroundTranslator
 
   public function callback($instance, $channelName, $message)
   {
-    echo $channelName, "==>", $message, PHP_EOL;
     $json = json_decode($message, true);
+    echo $channelName, ":", json_encode($json,JSON_PRETTY_PRINT), PHP_EOL;
     $this->translate($json['inputPath'], $json['outputPath'], $json['lang']);
   }
 
@@ -53,21 +45,26 @@ class DeeplBackgroundTranslator
       return;
     }
     try {
-      $this->createLock($outputDir);
-      $translator = new Translator($this->deeplApiKey);
-      echo "Starting translation for " . $inputPath, PHP_EOL;
-      $status = $translator->translateDocument($inputPath, $outputPath, null, $lang);
-      echo "Translation status: " . $status->status . " Errors: " . $status->errorMessage, PHP_EOL;
-    } catch (\Exception $e) {
-      echo "Error translating text: " . $e->getMessage(), PHP_EOL;
-    } finally {
-      $this->clearLock($outputDir);
+        if (!is_file($inputPath)){
+          echo "Input file $inputPath DOES NOT EXIST!!! EXITING!\n";
+          return;
+        }
+        $this->createLock($outputDir);
+        $this->printUsage();
+        echo "Starting translation for ".$inputPath, PHP_EOL;
+        $status = $this->translator->translateDocument($inputPath, $outputPath, null, $lang);
+        echo "Translation status: ".$status->status." Errors: ".$status->errorMessage, PHP_EOL;
+    }
+    catch (\DeepL\DocumentTranslationException $e){
+        echo "Error translating text: ".$e->getMessage(), PHP_EOL;
+    }
+    finally {
+        $this->clearLock($outputDir);
     }
   }
 
-  private function checkLock($outputDir)
-  {
-    $lockFile = $outputDir . "/translation.lock";
+  private function checkLock($outputDir){
+    $lockFile = $outputDir."/translation.lock";
 
     if (file_exists($lockFile)) {
       $lastModifiedTime = filemtime($lockFile);
@@ -89,8 +86,7 @@ class DeeplBackgroundTranslator
     }
   }
 
-  private function createLock($outputDir)
-  {
+  private function createLock($outputDir){
     $this->clearLock($outputDir);
     if (!file_exists($outputDir)) mkdir($outputDir, 0777, true);
     $lockFile = $outputDir . "/translation.lock";
@@ -98,10 +94,22 @@ class DeeplBackgroundTranslator
     echo "Lock created in " . $outputDir, PHP_EOL;
   }
 
-  private function clearLock($outputDir)
-  {
-    $lockFile = $outputDir . "/translation.lock";
+  private function clearLock($outputDir){
+    $lockFile = $outputDir."/translation.lock";
     if (file_exists($lockFile)) unlink($lockFile);
+  }
+
+  private function printUsage(){
+    $usage = $this->translator->getUsage();
+    if ($usage->anyLimitReached()) {
+        echo 'USAGE: Translation limit exceeded.\n';
+    }
+    if ($usage->character) {
+        echo 'USAGE: Characters: ' . $usage->character->count . ' of ' . $usage->character->limit."\n";
+    }
+    if ($usage->document) {
+        echo 'USAGE: Documents: ' . $usage->document->count . ' of ' . $usage->document->limit."\n";
+    }
   }
 }
 
